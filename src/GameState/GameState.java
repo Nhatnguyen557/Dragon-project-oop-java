d6133c39b8ec07069274e59260a27c7ef5369ff5
Nhatnguyen557 <nguyenvietnhat557@gmail.com>
2026-05-05 08:29:33 +0700

    first commit

---
+++ src/GameState/GameState.java
@@ -0,0 +1,20 @@
+package GameState;
+
+
+import TileMap.Background;
+
+public abstract  class GameState {
+
+    protected GameStateManager gsm;
+
+    private Background bg;
+
+    public abstract void init();
+    public abstract void update();
+    public abstract void draw(java.awt.Graphics2D g);
+    public abstract void keyPressed(int k);
+    public abstract void keyReleased(int k);
+
+
+
+}
